797028b7b8a6b63649dc43f689a76c9f5e4346e8
bwcd <txtdoc2023@163.com>
2024-04-08 11:17:46 +0800

    small change to my-test.sh

--- my-test.sh
+++ my-test.sh
@@ -6,5 +6,7 @@
 source /apps/software/anaconda3/etc/profile.d/conda.sh
 module load cuda/11.6
 conda activate torch39
-python main.py  --dir_data ../../datasets/CIFAR10  --batch-size 100  --epochs 2  --save weights/pretrain
+python main.py  --dir_data ../../datasets/CIFAR10  --batch-size 1000  --log-interval 10  --save weights/pretrain
 python vggprune.py  --dir_data ../../datasets/CIFAR10  --model weights/pretrain/checkpoint_VGG16bn_l1_norm.pth.tar  --save weights/prune
+python main_finetune.py  --dir_data ../../datasets/CIFAR10  --refine weights/prune/pruned.pth.tar  --batch-size 1000  --test-batch-size 1000  --log-interval 10  --save weights/finetune
+python test.py  --dir_data ../../datasets/CIFAR10  --test-batch-size 1000  --baseline weights/pretrain/checkpoint_VGG16bn_l1_norm.pth.tar  --pruned weights/prune/pruned.pth.tar  --finetune weights/finetune/finetune_checkpoint.pth.tar
