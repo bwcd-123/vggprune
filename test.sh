6d42dff86b0c65bfb0aac976a9c48bcccda37752
bwcd <txtdoc2023@163.com>
2024-04-08 08:31:03 +0800

    Add test.sh to run two python files once. Add error folder and output folder.

---
+++ test.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+#JSUB -q AI205009
+#JSUB -gpgpu 1
+#JSUB -e error/main.%J
+#JSUB -o output/main.%J
+source /apps/software/anaconda3/etc/profile.d/conda.sh
+module load cuda/11.6
+conda activate torch39
+python main.py  --dir_data ../../datasets/CIFAR10  --batch-size 100  --epochs 2  --save weights/pretrain
+python vggprune.py  --dir_data ../../datasets/CIFAR10  --model weights/pretrain/checkpoint_VGG16bn_l1_norm.pth.tar  --save weights/prune
