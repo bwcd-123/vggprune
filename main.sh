3bb8536dd2197d9bea61a577daa5e232e067b627
bwcd <txtdoc2023@163.com>
2024-04-08 09:58:51 +0800

    Add swanlab to record training logs. Also show the time elapsed per epoch.

--- main.sh
+++ main.sh
@@ -6,4 +6,4 @@
 source /apps/software/anaconda3/etc/profile.d/conda.sh
 module load cuda/11.6
 conda activate torch39
-python main.py  --dir_data ../../datasets/CIFAR10  --batch-size 100  --epochs 2  --save weights/pretrain
+python main.py  --dir_data ../../datasets/CIFAR10  --batch-size 1000  --epochs 2  --log-interval 10  --save weights/pretrain
