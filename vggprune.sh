f9f4579f3edabeedb850567da4e8c88107d3e065
bwcd <txtdoc2023@163.com>
2024-04-08 09:00:11 +0800

    Add two folders and modify sh files.

--- vggprune.sh
+++ vggprune.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 #JSUB -q AI205009
 #JSUB -gpgpu 1
-#JSUB -e error/error.%J
-#JSUB -o output/output.%J
+#JSUB -e error/vggprune.%J
+#JSUB -o output/vggprune.%J
 source /apps/software/anaconda3/etc/profile.d/conda.sh
 module load cuda/11.6
 conda activate torch39
